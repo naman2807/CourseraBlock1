411e8f2b51f565a03ed3871e3c6e5c9724fc5c74
naman2807 <namanagarwaldbs438@gmail.com>
2021-07-08 16:45:59 +0530

    Created pinkButton instance in MainActivity.java

--- app/src/main/java/com/example/courserablock1/MainActivity.java
+++ app/src/main/java/com/example/courserablock1/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     Button blueButton;
+    Button pinkButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
